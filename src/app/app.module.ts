bcfb134fa97c497e31e6313e67e2ce6e87a58b34
Lkabeer <ahmed@Lkabeer.com>
2017-09-25 06:31:10 +0200

    version End

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireAuthModule } from "angularfire2/auth";
+
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBMgIR0H4UFW772MNnd5vndZb4GMnqkOWQ",
+  authDomain: "angular4simpleapp.firebaseapp.com",
+  databaseURL: "https://angular4simpleapp.firebaseio.com",
+  projectId: "angular4simpleapp",
+  storageBucket: "angular4simpleapp.appspot.com",
+  messagingSenderId: "23751284563"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +25,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
